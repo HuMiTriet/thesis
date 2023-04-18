41bdd7fdfcd0f28c1b850aa19e61ddd20ec4002f
Triet Huynh <triet403@gmail.com>
2023-04-18 09:26:29 +0700

    WIP 500 server overloaded token ring

---
+++ client_token_ring/__init__.py
@@ -0,0 +1,72 @@
+from collections import defaultdict
+from enum import Enum
+import os
+
+
+from dataclasses import dataclass, field
+
+# from datetime import datetime, timedelta
+
+from dotenv import load_dotenv
+from flask import Flask
+from werkzeug.exceptions import InternalServerError
+
+
+class State(Enum):
+    DEFAULT = 0
+    REQUESTING = 1
+    EXECUTING = 2
+
+
+@dataclass()
+class ClientState:
+    current_state: defaultdict[str, State] = field(
+        default_factory=lambda: defaultdict(lambda: State.DEFAULT)
+    )
+    # tokens: set[str] = field(default_factory=set)
+
+    def get_next_client_url(self, host_url: str) -> str | None:
+        all_urls = os.getenv(
+            "TOKEN_RING",
+            """http://127.0.0.1:5002/ http://127.0.0.1:5003/
+                http://127.0.0.1:5004/ http://127.0.0.1:5005/""",
+        ).split(" ")
+
+        total_clients = len(all_urls)
+        for url_index, url in enumerate(all_urls):
+            if url == host_url:
+                return all_urls[(url_index + 1) % total_clients]
+
+        return None
+
+
+client_state = ClientState()
+
+
+# def get_session():
+#     if "session" not in g:
+#         g.session = aiohttp.ClientSession()
+#     return g.session
+
+
+def create_app():
+    """
+    The factory function that returns an instance of the client.
+
+    A client is an entity that interact with a resource in acquiring locks and
+    or releasing it.
+    """
+    load_dotenv()
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    # pylint: disable=import-outside-toplevel
+    from . import gossip
+
+    app.register_blueprint(gossip.bp)
+
+    @app.errorhandler(InternalServerError)
+    def custom_error_msg(error: InternalServerError):  # pyright: ignore
+        return f"CLIENT Err {str(error.__repr__)}", 500
+
+    return app
---
+++ client_token_ring/gossip.py
@@ -0,0 +1,108 @@
+import os
+
+# from datetime import datetime
+from flask import Blueprint, request
+import aiohttp
+
+
+from . import State, client_state
+
+
+TIMEOUT: float = float(os.getenv("TIMEOUT", "2"))
+
+SERVER_URL: str = os.getenv("SERVER_URL", "http://127.0.0.1:5000/")
+
+REGISTRAR_URL: str = os.getenv("REGISTRAR_URL", "http://127.0.0.1:5001/")
+
+CLIENT_URL: str = os.getenv("CLIENT_URL", "http://127.0.0.1:5004")
+
+TOTAL_CLIENT: str = os.getenv("TOTAL_CLIENT", "3")
+
+PROXY_URL: str = os.getenv("PROXY_URL", "http://127.0.0.1:5001")
+
+bp = Blueprint("gossip", __name__)
+
+
+@bp.route("/<string:resource_id>/request", methods=["POST"])
+async def request_resource(resource_id: str) -> tuple[str, int]:
+    resource_cur_state = client_state.current_state[resource_id]
+
+    if resource_cur_state == State.EXECUTING:
+        return f"client is already executing resource {resource_id}", 200
+
+    if resource_cur_state == State.DEFAULT:
+        resource_cur_state = State.REQUESTING
+
+    return f"client has requested {resource_id}", 200
+
+
+@bp.route("/<string:resource_id>/lock", methods=["DELETE"])
+async def delete_request(resource_id: str) -> tuple[str, int]:
+
+    resource_cur_state = client_state.current_state[resource_id]
+
+    if resource_cur_state == State.DEFAULT:
+        return (
+            f"client cannot delete {resource_id} lock because has not yet hold it",
+            401,
+        )
+
+    resource_cur_state = State.DEFAULT
+
+    if resource_cur_state == State.EXECUTING:
+        _, code = await release_lock(resource_id)
+        return f"client EXECUTING {resource_id} to DEFAULT)", code
+
+    return f"client REQUESTING {resource_id} to DEFAULT)", 200
+
+
+@bp.route("/<string:resource_id>/token", methods=["PUT"])
+async def receive_token(resource_id: str) -> tuple[str, int]:
+
+    resource_cur_state = client_state.current_state[resource_id]
+
+    if resource_cur_state == State.DEFAULT:
+        resp, code = await pass_token(resource_id)
+        return resp, code
+
+    if resource_cur_state == State.REQUESTING:
+        resource_cur_state = State.EXECUTING
+        resp, code = await lock_resource(resource_id)
+        return resp, code
+
+    # The only state remaining is EXECUTING but if the client is executing then
+    # it is currently holding that token so it can't receive a token from somewhere else
+    return f"duplication of token for {resource_id}", 400
+
+
+async def pass_token(resource_id: str) -> tuple[str, int]:
+    next_url = client_state.get_next_client_url(request.host_url)
+    async with aiohttp.ClientSession() as session:
+        async with session.put(f"{next_url}{resource_id}/token") as response:
+            return (
+                f"client has passed token {resource_id} to {next_url}",
+                response.status,
+            )
+
+
+async def lock_resource(resource_id: str) -> tuple[str, int]:
+    async with aiohttp.ClientSession() as session:
+        async with session.put(
+            f"{SERVER_URL}{resource_id}/lock",
+            json={
+                "origin": request.host_url,
+            },
+            proxy=PROXY_URL,
+        ) as response:
+            return await response.text(), response.status
+
+
+async def release_lock(resource_id: str) -> tuple[str, int]:
+    async with aiohttp.ClientSession() as session:
+        async with session.delete(
+            f"{SERVER_URL}{resource_id}/lock",
+            json={},
+            proxy=PROXY_URL,
+            timeout=TIMEOUT,
+        ) as response:
+            return await response.text(), response.status
